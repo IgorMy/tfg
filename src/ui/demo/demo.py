05b2aeadf25dcb4a5425e537f23f66cffb727747
Ihar Myshkevich <ihar.myshkevich134@alu.uhu.es>
2024-06-23 23:05:00 +0200

    🩺 Alt added to images

--- src/ui/demo/demo.py
+++ src/ui/demo/demo.py
@@ -5,12 +5,12 @@ def image_banner():
     with ui.element("div").classes(
         "flex flex-row justify-center items-center justify-between px-10 gap-4"
     ):
-        ui.element("img").props('src="/static/images/base_demostration.jpg"').classes(
-            "rounded-md w-[95%] sm:w-[40%]"
-        )
+        ui.element("img").props(
+            'src="/static/images/base_demostration.jpg" alt="Base image"'
+        ).classes("rounded-md w-[95%] sm:w-[40%]")
         ui.icon("r_arrow_forward").classes(
             "hidden sm:!inline-flex text-4xl text-gray-600 dark:text-gray-300 "
         ).style("margin: 0 20px;")
-        ui.element("img").props('src="/static/images/image_with_mask.jpg"').classes(
-            "rounded-md w-[95%] sm:w-[40%]"
-        )
+        ui.element("img").props(
+            'src="/static/images/image_with_mask.jpg" alt="Mask image"'
+        ).classes("rounded-md w-[95%] sm:w-[40%]")
